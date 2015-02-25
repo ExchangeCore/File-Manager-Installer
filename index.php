63a83dc0cc6cafe0d2a6d05be58ab36a7dea7740
joemeyer@exchangecore.com <joemeyer@exchangecore.com>
2015-02-25 10:12:38 -0500

    updated index and composer

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
 require_once ('vendor/autoload.php');
 require_once ('vendor/yiisoft/yii2/Yii.php');
 
-$applicationConfig = include __DIR__ . '/application/config/main.php';
+if (file_exists(__DIR__ . '/application/config/main.php')) {
+    $applicationConfig = include __DIR__ . '/application/config/main.php';
+} else {
+    $applicationConfig = array();
+}
 
 new exchangecore\filemanager\Bootstrap($applicationConfig);
